301b268119699b0fa4c2c43a9493b2eeb4f7b5eb
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-07-07 11:54:58 +0400

    continuin work

--- build/js/script.js
+++ build/js/script.js
@@ -7,3 +7,10 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+let newBookBtn = document.querySelector("#myButton");
+newBookBtn.addEventListener("click", function () {
+  let newBookForm = document.querySelector("#newForm");
+  let overlay = document.querySelector("#overlay");
+//   newBookForm.style.display = "block";
+});
