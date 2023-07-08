b526c67661760f63481e12fa750007709fe9b6d2
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-07-08 22:13:34 +0400

    Almost finished

--- build/js/script.js
+++ build/js/script.js
@@ -1,16 +1,97 @@
+"use strict";
+
+const newBookBtn = document.querySelector("#newBookBtn");
+const overlay = document.querySelector("#overlay");
+const form = document.querySelector("#form");
+const formCloseBtn = document.getElementById("formCloseBtn");
+const newBookForm = document.getElementById("newForm");
+
+// form labels
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
+
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function closeTheForm() {
+  overlay.classList.add("hidden");
+  form.classList.add("hidden");
+  resetForm();
+}
+
+function render() {
+  const container = document.querySelector("#libraryContainer");
+
+  // Clear the existing content of the grid
+  container.innerHTML = "";
+
+  // Iterate over each book in myLibrary array
+  myLibrary.forEach((book) => {
+    // Create an HTML template for the book element
+    const bookElement = `
+    <div class="flex flex-col justify-between gap-5 p-5 rounded-lg bg-white shadow-lg z-10 text-xl">
+      <h3 class="flex justify-center text-2xl font-semibold">${book.title}</h3>
+      <p class="flex justify-center text-base font-semibold ">${book.author}</p>
+      <p class="flex justify-center text-base font-semibold ">${book.pages} pages</p>
+      <p class="flex justify-center text-base font-semibold ">${book.read ? "Read" : "Not read"}</p>
+    </div>
+    `;
+
+    // Append the book element to the grid
+    container.insertAdjacentHTML("beforeend", bookElement);
+  });
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title_ = title.value;
+  let author_ = author.value;
+  let pages_ = pages.value;
+  let read_ = read.checked;
+  let newBook = new Book(title_, author_, pages_, read_);
+  myLibrary.push(newBook);
+  // console.log(myLibrary);
+
+  render();
+
+  // resetForm();
+  closeTheForm();
 }
 
-let newBookBtn = document.querySelector("#myButton");
+function resetForm() {
+  newBookForm.reset();
+}
+
+// EventListeners to manipulate Add Book
 newBookBtn.addEventListener("click", function () {
-  let newBookForm = document.querySelector("#newForm");
-  let overlay = document.querySelector("#overlay");
-//   newBookForm.style.display = "block";
+  overlay.classList.toggle("hidden");
+  form.classList.remove("hidden");
+});
+
+overlay.addEventListener("click", function () {
+  closeTheForm();
+});
+
+formCloseBtn.addEventListener("click", function () {
+  closeTheForm();
+});
+
+// EventListener for Esc key press
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closeTheForm();
+  }
+});
+
+// EventListener for adding new object
+newBookForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  addBookToLibrary();
 });
